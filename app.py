215951478a6e5339b470ff3355ac5ef26f0ecaf5
David mahannah <mahannahd@gmail.com>
2022-02-16 09:02:56 -0600

    quick fix

--- app.py
+++ app.py
@@ -1752,9 +1752,9 @@ def letsapplogin(username, password):
 		return jsonify({'status': 'FAILED'})
 	
 
-#if __name__ == "__main__":
-#    tz = pytz.timezone('America/Chicago')
-#    #manager.run()
-#    app.run(host='0.0.0.0')
+if __name__ == "__main__":
+    tz = pytz.timezone('America/Chicago')
+    #manager.run()
+    app.run()
 
 
